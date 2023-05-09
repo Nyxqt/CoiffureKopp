be799256bbc75a08317aa3640578eb6a31d47654
Nyxqt <124267408+Nyxqt@users.noreply.github.com>
2023-05-09 17:34:32 +0200

    not much + bugged will fix tomorrow cba

--- src/templates/front/homepage.php
+++ src/templates/front/homepage.php
@@ -1,7 +1,29 @@
 <?php $title = "Accueil"; ?>
 <?php ob_start(); ?>
 
-coucou
+
+<div id="carousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="../assets/images/femme_nb.jpg" class="d-block" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
 <div class="test"></div>
 
 <?php $content = ob_get_clean(); ?>
