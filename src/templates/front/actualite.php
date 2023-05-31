4f30a1e9127c1ee1c18cb11a9fb8b5de8c5c068a
Nyxqt <124267408+Nyxqt@users.noreply.github.com>
2023-05-31 16:21:37 +0200

    news page working

--- src/templates/front/actualite.php
+++ src/templates/front/actualite.php
@@ -18,7 +18,7 @@
                 <div class="col-lg-6 text-center">
                     <h1 class="title-ff"><?= $actu['post_title'] ?></h1>
                     <p class="text-ff fs-5 fst-italic">Publié le <?= $actu['post_date'] ?>.</p>
-                    <a href="index.php?action=actualite&id=<?= $actu['post_id'] ?>" class="btn btn-dark fs-4 mt-5" role="button" data-bs-toggle="button">Voir l'article</a>
+                    <a href="index.php?action=actualite&id=<?= $actu['post_id'] ?>" class="btn btn-dark fs-4 mt-5">Voir l'article</a>
                 </div>
             </div>
         <?php } ?>
--- src/templates/front/post.php
+++ src/templates/front/post.php
@@ -3,13 +3,21 @@
 <!-- HIGHLIGHT -->
 <section>
     <div class="container parallax-salon" id="salon-highlight">
-        <h1 class="highlight-ff text-center text-white">L'EQUIPE</h1>
+        <h1 class="highlight-ff text-center text-white">ARTICLE</h1>
     </div>
 </section>
 <!-- MAIN CONTENT -->
 <section class="mt-5">
     <div class="container-lg">
-
+        <h1 class="title-ff text-center"><?= $post[0]['post_title'] ?></h1>
+        <div class="row">
+            <div class="col-lg-6">
+                <img class="w-100" src="<?= $post[0]['post_path'] ?>">
+            </div>
+            <div class="col-lg-6">
+                <p class="fs-5 text-ff"><?= $post[0]['post_content'] ?></p>
+            </div>
+        </div>
     </div>
 </section>
 
