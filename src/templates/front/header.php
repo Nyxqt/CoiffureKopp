f8d35427c8bc09ad4a5eac54ec689469c9c483dc
Nyxqt <124267408+Nyxqt@users.noreply.github.com>
2023-06-20 16:45:03 +0200

    update  responsive

--- src/templates/front/header.php
+++ src/templates/front/header.php
@@ -1,15 +1,16 @@
 <?php ob_start(); ?>
 
-<nav class="navbar navbar-expand-md">
-    <div class="container-md">
+<nav data-bs-theme="dark" class="navbar  navbar-expand-lg">
+    <div class="container-fluid d-flex flex-column text-center">
+        <a class="navbar-brand p-0 m-0 mb-2" href="index.php">
+            <img src="../assets/images/placeholder.jpg" alt="Logo" width="60" height="60" class="mt-3">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex flex-column" id="navbarCollapse">
-            <a class="navbar-brand" href="index.php">
-                <img src="../assets/images/placeholder.jpg" alt="Logo" width="60" height="60" class="mt-3">
-            </a>
-            <ul class="navbar-nav justify-content-around w-100 text-white">
+        <div class="collapse navbar-collapse justify-content-center" id="navbarCollapse">
+
+            <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link title-ff title-fs nav-style" href="index.php?action=salon">Le salon</a>
                 </li>
