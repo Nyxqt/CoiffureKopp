6ac4894794eac05fc4e2a5bff9a4349f82ebdd65
Nyxqt <124267408+Nyxqt@users.noreply.github.com>
2023-06-27 16:22:40 +0200

    update

--- src/assets/JS/navbar.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbarCollapse").style.padding = "15px 0";
-  } else {
-    document.getElementById("navbarCollapse").style.padding = "30px 0";
-  }
-}
--- src/templates/front/homepage.php
+++ src/templates/front/homepage.php
@@ -2,24 +2,27 @@
 <?php ob_start(); ?>
 <!-- CAROUSEL -->
 <section>
-    <div id="carousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="../assets/images/carousel1.jpg" class="d-block w-100" alt="...">
+    <div class="container-md">
+        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+            <div id="carouselDiv" class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../assets/images/carousel-1.jpg" class="carousel-img" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/images/carousel2.jpg" class="carousel-img" alt="...">
+                </div>
             </div>
-            <div class="carousel-item">
-                <img src="../assets/images/carousel2.jpg" class="d-block w-100" alt="...">
-            </div>
+            <button class="carousel-control-prev align-self-center" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Précédent</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Suivant</span>
+            </button>
         </div>
-        <button class="carousel-control-prev align-self-center" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Précédent</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Suivant</span>
-        </button>
     </div>
+    
 </section>
 <!-- MAIN CONTENT -->
 <section class=" mt-5">
@@ -35,7 +38,9 @@
                 <div class="text-divider">
                     <h2 class="title-ff">Une expérience</h2>
                 </div>
-                <p class="text-ff fs-3">Je sais pas du coup mange du lorem : Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, atque ipsa. Quis omnis sit voluptas impedit inventore laudantium quisquam, pariatur est totam vitae ducimus neque quos veritatis iusto assumenda architecto?</p>
+                <p class="text-ff fs-3">Un salon de coiffure différent, où l’on se sent comme chez soi. <br />
+                Le professionnalisme et les conseils d’une équipe de coiffeurs passionnés <br />
+                Une coupe qui révèle votre personnalité et vous donne le sourire !</p>
             </div>
             <div class="col-md-6">
                 <img src="../assets/images/femme_nb.jpg" class="rounded content-img mx-auto d-block" alt="">
